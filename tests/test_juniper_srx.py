d6c1de2cf88ada6fc02f4a80cfc69b96efba5717
Tyler Christiansen <code@tylerc.me>
2015-03-13 12:56:06 -0700

    refactor juniper tests further, placing commands with the methods that test them

--- tests/test_juniper_srx.py
+++ tests/test_juniper_srx.py
@@ -29,50 +29,47 @@ def setup_module(module):
     dispatcher = dispatcher["juniper"]
 
     SSHClass = netmiko.ssh_dispatcher(dispatcher["device_type"])
-    net_connect = SSHClass(**dispatcher)
-
-    module.show_version = net_connect.send_command(module.commands["version"])
-    module.multiple_line_output = net_connect.send_command(module.commands["multiline"],
-                                                           delay_factor=2)
-    module.show_ip = net_connect.send_command(module.commands["basic"])
-    module.base_prompt = net_connect.base_prompt
-
-    # Test buffer clearing
-    net_connect.remote_conn.send(module.commands["version"])
-    time.sleep(2)
-    net_connect.clear_buffer()
-    # Should not be anything there on the second pass
-    module.clear_buffer_check = net_connect.clear_buffer()
+    module.net_connect = SSHClass(**dispatcher)
 
 def test_disable_paging():
     """Verify paging is disabled by looking for string after when paging would normally occur."""
+    multiple_line_output = net_connect.send_command(commands["multiline"], delay_factor=2)
     assert 'security-zone untrust' in multiple_line_output
 
 def test_verify_ssh_connect():
     """Verify the connection was established successfully."""
+    show_version = net_connect.send_command(commands["version"])
     assert 'JUNOS Software Release' in show_version
 
 def test_verify_send_command():
     """Verify a command can be sent down the channel successfully."""
-    assert EXPECTED_RESPONSES['interface_ip'] in show_ip
+    show_ip = net_connect.send_command(commands["basic"])
+    assert EXPECTED_RESPONSES["interface_ip"] in show_ip
 
 def test_base_prompt():
     """Verify the router base_prompt is detected correctly."""
-    assert base_prompt == EXPECTED_RESPONSES['base_prompt']
+    base_prompt = net_connect.base_prompt
+    assert base_prompt == EXPECTED_RESPONSES["base_prompt"]
 
 def test_strip_prompt():
     """Ensure the router prompt is not in the command output."""
-    assert EXPECTED_RESPONSES['base_prompt'] not in show_version
+    show_version = net_connect.send_command(commands["version"])
+    assert EXPECTED_RESPONSES["base_prompt"] not in show_version
 
 def test_strip_command():
     """Ensure that the command that was executed does not show up in the command output."""
+    show_ip = net_connect.send_command(commands["basic"])
     assert commands["basic"] not in show_ip
 
 def test_normalize_linefeeds():
     """Ensure no '\r' sequences."""
+    show_ip = net_connect.send_command(commands["basic"])
     assert not '\r' in show_ip
 
 def test_clear_buffer():
     """Test that clearing the buffer works."""
+    net_connect.remote_conn.send(commands["version"])
+    clear_buffer_check = net_connect.clear_buffer()
+    clear_buffer_check = net_connect.clear_buffer()
     assert clear_buffer_check is None
 
